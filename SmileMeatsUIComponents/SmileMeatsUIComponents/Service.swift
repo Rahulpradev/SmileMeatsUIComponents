ae828a3d8837850e5baf342f9d138cc7a3db1ee7
Rahul Pradev <rahulmysterio1212@gmail.com>
2022-04-29 16:59:48 +0530

    Finished our  framework

---
+++ SmileMeatsUIComponents/SmileMeatsUIComponents/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  SmileMeatsUIComponents
+//
+//  Created by RAHUL PRADEV on 29/04/22.
+//
+
+import Foundation
+
+class Service {
+    func printSomething() -> String {
+        return "Did my job"
+    }
+}
---
+++ SmileMeatsUIComponents/SmileMeatsUIComponents/SmileMeatsUIComponents.h
@@ -0,0 +1,18 @@
+//
+//  SmileMeatsUIComponents.h
+//  SmileMeatsUIComponents
+//
+//  Created by RAHUL PRADEV on 29/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmileMeatsUIComponents.
+FOUNDATION_EXPORT double SmileMeatsUIComponentsVersionNumber;
+
+//! Project version string for SmileMeatsUIComponents.
+FOUNDATION_EXPORT const unsigned char SmileMeatsUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmileMeatsUIComponents/PublicHeader.h>
+
+
